6d0aa448c82c1cf414121ab288dc2812afee6c21
Peter Paine <peter@pppaine.com>
2021-11-29 14:27:40 -0500

    Added 3-band splitting

--- Source/PluginProcessor.h
+++ Source/PluginProcessor.h
@@ -157,15 +157,19 @@ private:
     CompressorBand compressor;
 
     using Filter = juce::dsp::LinkwitzRileyFilter<float>;
-    Filter LP, HP;
+   
+    //     fc0  fc1
+    Filter LP1, AP2,
+           HP1, LP2,
+                HP2;
 
-    Filter AP;
+    //Filter invAP1, invAP2;
+    //juce::AudioBuffer<float> invAPBuffer;
 
-    juce::AudioBuffer<float> apBuffer;
-
-    juce::AudioParameterFloat* lowCrossover{ nullptr };
+    juce::AudioParameterFloat* lowMidCrossover{ nullptr };
+    juce::AudioParameterFloat* midHighCrossover{ nullptr };
     
-    std::array<juce::AudioBuffer<float>, 2> filterBuffers;
+    std::array<juce::AudioBuffer<float>, 3> filterBuffers;
 
     //==============================================================================
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (SimpleMBCompAudioProcessor)
